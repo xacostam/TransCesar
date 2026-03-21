0d94108d2ac6e7fd2710f60f7d03a997e30426df
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-03-20 20:33:54 -0500

    feat:se creo la clase personaservice y la validaciones del metodo registrar pasajero

--- src/transcesar/service/PersonaService.java
+++ src/transcesar/service/PersonaService.java
@@ -4,10 +4,14 @@
  */
 package transcesar.service;
 
-/**
- *
- * @author xavia
- */
+import transcesar.dao.PersonaDao;
+
 public class PersonaService {
-    
+     PersonaDao dao = new PersonaDao();
+    public void registrarPasajero(String tipo, String nombre, String documento, double descuento) {
+
+        String datos = tipo + ";" + nombre + ";" + documento + ";" + descuento;
+
+        dao.registrarPasajero(datos);
+    }
 }
