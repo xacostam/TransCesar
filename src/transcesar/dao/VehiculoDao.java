687a9a84704f3249590b96679acb044f729a29bf
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-03-21 18:21:37 -0500

    fix: ajustes antes de rebase

--- src/transcesar/dao/VehiculoDao.java
+++ src/transcesar/dao/VehiculoDao.java
@@ -4,8 +4,7 @@
  */
 package transcesar.dao;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -14,25 +13,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author xavia
- */
 public class VehiculoDao {
-   
-=======
-import java.io.FileWriter;
-import java.io.IOException;
-=======
-import java.io.*;
-<<<<<<< HEAD
-import java.util.*;
->>>>>>> 6dc9b94 (feat:se creo el metodo listarAuto)
-=======
->>>>>>> ff2051d (feat: se creo la clase vehiculoDao y el metodo registrarAuto)
 
-public class VehiculoDao {
->>>>>>> 499ed3f (feat: se creo la clase vehiculoDao y el metodo registrarAuto)
     public void registrarAuto(String archivo, String datos) {
 
         try {
@@ -44,12 +26,6 @@ public class VehiculoDao {
         }
 
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-=======
->>>>>>> 6dc9b94 (feat:se creo el metodo listarAuto)
     public List<String> listarAuto(String archivo) {
 
         List<String> lista = new ArrayList<>();
@@ -72,13 +48,7 @@ public class VehiculoDao {
     }
     
     public String buscarAuto(String archivo, String placa) {
-<<<<<<< HEAD
-
         try {
-
-=======
-        try {
->>>>>>> 6dc9b94 (feat:se creo el metodo listarAuto)
             BufferedReader br = new BufferedReader(new FileReader(archivo));
             String linea;
 
@@ -88,8 +58,6 @@ public class VehiculoDao {
                     br.close();
                     return linea;
                 }
-<<<<<<< HEAD
-
             }
 
             br.close();
@@ -100,62 +68,6 @@ public class VehiculoDao {
 
         return null;
     }
-    public void cambiarEstado(String archivo, String placa, String nuevoEstado) {
-
-        try {
-
-            File file = new File(archivo);
-            List<String> lineas = new ArrayList<>();
-
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String linea;
-
-            while ((linea = br.readLine()) != null) {
-
-                if (linea.contains(placa)) {
-
-                    String[] datos = linea.split(";");
-                    datos[2] = nuevoEstado;
-
-                    linea = String.join(";", datos);
-                }
-
-                lineas.add(linea);
-            }
-
-            br.close();
-
-            FileWriter fw = new FileWriter(file);
-
-            for (String l : lineas) {
-                fw.write(l + "\n");
-            }
-
-            fw.close();
-
-        } catch (IOException e) {
-
-            System.out.println("Error cambiando estado");
-
-        }
-
-    }
-    
-    
-=======
->>>>>>> 499ed3f (feat: se creo la clase vehiculoDao y el metodo registrarAuto)
-=======
-            }
-            br.close();
-        } catch (IOException e) {
-            System.out.println("Error buscando vehículo");
-        }
-        return null;
-    }
-<<<<<<< HEAD
->>>>>>> 6dc9b94 (feat:se creo el metodo listarAuto)
-=======
-    
      public void cambiarEstado(String archivo, String placa, String nuevoEstado) {
          
         try {
@@ -183,7 +95,4 @@ public class VehiculoDao {
             System.out.println("Error cambiando estado");
         }
     }
->>>>>>> 72da723 (feat:se creo el metodo cambiarestados)
-=======
->>>>>>> ff2051d (feat: se creo la clase vehiculoDao y el metodo registrarAuto)
 }
