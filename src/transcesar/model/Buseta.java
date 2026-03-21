bf213ed72bfe3ce2cd2c83d9e922858eaf706452
xacosta <xacosta@unicesar.edu.co>
2026-03-20 22:57:46 -0500

    feat: se creo la clase buseta y sus atributos

--- src/transcesar/model/Buseta.java
+++ src/transcesar/model/Buseta.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package transcesar.model;
 
-/**
- *
- * @author xavia
- */
-public class Buseta {
-    
+
+public class Buseta extends Vehiculo {
+
+    private int tarifaBase = 8000;
+
+    public Buseta(String placa, String ruta, String estado) {
+        super(placa, ruta, estado, 19);
+    }
+
+    public int getTarifaBase() {
+        return tarifaBase;
+    }
 }
