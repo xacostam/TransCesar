055be42b744f80d20f5c21ad01f63a20659a281b
https://github.com/sebastianalopez-a11y <Sebastianalopez@unicesar.edu.co>
2026-03-21 19:56:01 -0500

    fix:se hizo la correcion a un error que se de la clase abtracta pasajero, en el construtor

--- src/transcesar/model/Pasajero.java
+++ src/transcesar/model/Pasajero.java
@@ -1,25 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package transcesar.model;
 
 import java.time.LocalDate;
 import java.time.Period;
 
-public abstract class Pasajero {
-
-    protected String nombre;
+public abstract class Pasajero extends Persona {
+    
+    public Pasajero(String nombre, String documento) {
+        super(nombre, documento);
+    }
+     protected String nombre;
     protected String documento;
     protected LocalDate fechaNacimiento;
     protected double descuento;
 
     public Pasajero(String nombre, String documento, LocalDate fechaNacimiento) {
-
-        this.nombre = nombre;
-        this.documento = documento;
+          super(nombre, documento);
         this.fechaNacimiento = fechaNacimiento;
-
         this.descuento = calcularDescuento();
     }
 
